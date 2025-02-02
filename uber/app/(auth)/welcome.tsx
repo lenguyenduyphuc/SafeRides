e9e93baa0c795010865973d6b6a0a6a85dee507c
Phuc <duyphuclenguyen@gmail.com>
2025-02-02 17:01:20 -0500

    Fix minor bug

--- uber/app/(auth)/welcome.tsx
+++ uber/app/(auth)/welcome.tsx
@@ -1,10 +1,42 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Text } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
+import { router } from "expo-router";
+import Swiper from "react-native-swiper";
+import { useRef, useState } from "react";
+import { onboarding } from "@/constants";
 
 const Onboarding = () => {
+  const swiperRef = useRef<Swiper>(null);
+  const [active, setActive] = useState(0);
+
+  console.log(onboarding);
   return (
-    <SafeAreaView>
-      <Text>Onboarding</Text>
+    <SafeAreaView className="flex h-full items-center justify-between bg-white">
+      <TouchableOpacity
+        onPress={() => {
+          router.replace("/(auth)/sign-up");
+        }}
+        className="w-full flex justify-end items-end p-5"
+      >
+        <Text className="text-red text-md font-JakartaBold ">Skip</Text>
+      </TouchableOpacity>
+      <Swiper
+        ref={swiperRef}
+        loop={false}
+        dot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full" />
+        }
+        activeDot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full " />
+        }
+        onIndexChanged={(index) => setActive(index)}
+      >
+        {onboarding.map((item, index) => (
+          <View key={index}>
+            <Text>{item.title}</Text>
+          </View>
+        ))}
+      </Swiper>
     </SafeAreaView>
   );
 };
