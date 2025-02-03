52933de2a7308e431a0abb091aaf4b54cff1dcd5
Phuc <duyphuclenguyen@gmail.com>
2025-02-03 11:05:16 -0500

    Finish the get started menu

---
+++ uber/components/CustomButton.tsx
@@ -0,0 +1,58 @@
+import { TouchableOpacity, Text, ButtonProps } from "react-native";
+
+const getBgVariantStyle = (variant: ButtonProps["bgVariant"]) => {
+  switch (variant) {
+    case "secondary":
+      return "bg-gray-500";
+    case "danger":
+      return "red-gray-500";
+    case "success":
+      return "green-gray-500";
+    case "outline":
+      return "bg-transparent border-neutral-300 border-[0.5px]";
+    default:
+      return "bg-[#0286ff]";
+  }
+};
+
+const getTextVariantStyle = (variant: ButtonProps["textVariant"]) => {
+  switch (variant) {
+    case "primary":
+      return "text-black";
+    case "secondary":
+      return "text-gray-100";
+    case "danger":
+      return "text-red-100";
+    case "success":
+      return "text-green-100";
+    default:
+      return "text-white-100";
+  }
+};
+
+const CustomButton = ({
+  onPress,
+  title,
+  bgVariant = "primary",
+  textVariant = "default",
+  IconLeft,
+  IconRight,
+  className,
+  ...props
+}: ButtonProps) => (
+  <TouchableOpacity
+    onPress={onPress}
+    className={`w-full rounded-full p-4
+  flex flex-row justify-center items-center shadow-md
+  shadow-neutral-100/70 ${getBgVariantStyle(bgVariant)} ${className}`}
+    {...props}
+  >
+    {IconLeft && <IconLeft />}
+    <Text className={`text-lg font-bold ${getTextVariantStyle(textVariant)}`}>
+      {title}
+    </Text>
+    {IconRight && <IconRight />}
+  </TouchableOpacity>
+);
+
+export default CustomButton;
