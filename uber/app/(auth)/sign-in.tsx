c1efff60793e80bf663b9ccd1bf27a9624de0a30
Phuc <duyphuclenguyen@gmail.com>
2025-02-05 21:58:02 -0500

    Small update, tailwindcss is breaking

--- uber/app/(auth)/sign-in.tsx
+++ uber/app/(auth)/sign-in.tsx
@@ -1,15 +1,15 @@
-import { Text, View, Image, ScrollView } from "react-native";
-import { icons, images } from "@/constants";
-import InputField from "@/components/InputField";
-import { useState, useCallback } from "react";
-import CustomButton from "@/components/CustomButton";
-import { Link, useRouter } from "expo-router";
-import OAuth from "@/components/OAuth";
 import { useSignIn } from "@clerk/clerk-expo";
+import { Link, router } from "expo-router";
+import { useCallback, useState } from "react";
+import { Alert, Image, ScrollView, Text, View } from "react-native";
+
+import CustomButton from "@/components/CustomButton";
+import InputField from "@/components/InputField";
+import OAuth from "@/components/OAuth";
+import { icons, images } from "@/constants";
 
 const SignIn = () => {
   const { signIn, setActive, isLoaded } = useSignIn();
-  const router = useRouter();
 
   const [form, setForm] = useState({
     email: "",
@@ -19,75 +19,73 @@ const SignIn = () => {
   const onSignInPress = useCallback(async () => {
     if (!isLoaded) return;
 
-    // Start the sign-in process using the email and password provided
     try {
       const signInAttempt = await signIn.create({
         identifier: form.email,
         password: form.password,
       });
 
-      // If sign-in process is complete, set the created session as active
-      // and redirect the user
       if (signInAttempt.status === "complete") {
         await setActive({ session: signInAttempt.createdSessionId });
-        router.replace("/");
+        router.replace("/(root)/(tabs)/home");
       } else {
-        // If the status isn't complete, check why. User might need to
-        // complete further steps.
-        console.error(JSON.stringify(signInAttempt, null, 2));
+        // See https://clerk.com/docs/custom-flows/error-handling for more info on error handling
+        console.log(JSON.stringify(signInAttempt, null, 2));
+        Alert.alert("Error", "Log in failed. Please try again.");
       }
-    } catch (err) {
-      // See https://clerk.com/docs/custom-flows/error-handling
-      // for more info on error handling
-      console.error(JSON.stringify(err, null, 2));
+    } catch (err: any) {
+      console.log(JSON.stringify(err, null, 2));
+      Alert.alert("Error", err.errors[0].longMessage);
     }
-  }, [isLoaded, form.email, form.password]);
+  }, [isLoaded, form]);
 
   return (
     <ScrollView className="flex-1 bg-white">
       <View className="flex-1 bg-white">
         <View className="relative w-full h-[250px]">
           <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
-          <Text
-            className="text-2xl text-black font-JakartaSemiBold 
-          absolute bottom-5 left-5"
-          >
-            Sign In your account
+          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+            Welcome 👋
           </Text>
         </View>
+
         <View className="p-5">
           <InputField
-            label="Enter Email"
-            placeholder="Enter your email"
-            placeholderTextColor="#888"
+            label="Email"
+            placeholder="Enter email"
             icon={icons.email}
+            textContentType="emailAddress"
             value={form.email}
             onChangeText={(value) => setForm({ ...form, email: value })}
           />
+
           <InputField
             label="Password"
-            placeholder="Enter your password"
-            placeholderTextColor="#888"
+            placeholder="Enter password"
             icon={icons.lock}
             secureTextEntry={true}
+            textContentType="password"
             value={form.password}
             onChangeText={(value) => setForm({ ...form, password: value })}
           />
+
+          <CustomButton
+            title="Sign In"
+            onPress={onSignInPress}
+            className="mt-6"
+          />
+
+          <OAuth />
+
+          <Link
+            href="/sign-up"
+            className="text-lg text-center text-general-200 mt-10"
+          >
+            Don't have an account?{" "}
+            <Text className="text-primary-500">Sign Up</Text>
+          </Link>
         </View>
-        <CustomButton
-          title="Sign In"
-          onPress={onSignInPress}
-          className="mt-6"
-        />
       </View>
-      <OAuth />
-      <Link
-        href="/sign-up"
-        className="text-lg text-center text-general-200 mt-10"
-      >
-        <Text>Don't have an account? </Text>
-        <Text className="text-primary-500">Sign Up</Text>
-      </Link>
     </ScrollView>
   );
 };
--- uber/app/_layout.tsx
+++ uber/app/_layout.tsx
@@ -3,10 +3,13 @@ import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import "react-native-reanimated";
-import "../global.css";
+import { LogBox } from "react-native";
 
 import { tokenCache } from "@/cache";
 import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 
@@ -16,7 +19,7 @@ if (!publishableKey) {
   );
 }
 
-SplashScreen.preventAutoHideAsync();
+LogBox.ignoreLogs(["Clerk:"]);
 
 export default function RootLayout() {
   const [loaded] = useFonts({
@@ -25,7 +28,7 @@ export default function RootLayout() {
     "Jakarta-ExtraLight": require("../assets/fonts/PlusJakartaSans-ExtraLight.ttf"),
     "Jakarta-Light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
     "Jakarta-Medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
-    "Jakarta-Regular": require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+    Jakarta: require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
     "Jakarta-SemiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
   });
 
@@ -44,8 +47,8 @@ export default function RootLayout() {
       <ClerkLoaded>
         <Stack>
           <Stack.Screen name="index" options={{ headerShown: false }} />
-          <Stack.Screen name="(root)" options={{ headerShown: false }} />
           <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          <Stack.Screen name="(root)" options={{ headerShown: false }} />
           <Stack.Screen name="+not-found" />
         </Stack>
       </ClerkLoaded>
