6c2a2cb73515de3a538183432713f9de5fd87d7a
Phuc <duyphuclenguyen@gmail.com>
2025-02-08 14:13:13 -0500

    Make the map working

--- uber_app/components/Map.tsx
+++ uber_app/components/Map.tsx
@@ -1,21 +1,50 @@
+import { useLocationStore } from "@/store";
 import { View, Text } from "react-native";
 import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
+import { calculateRegion } from "@/lib/map";
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+    width: "100%",
+    borderRadius: 16,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
 
 const Map = () => {
-  const region = {};
+  const {
+    userLongitude,
+    userLatitude,
+    destinationLatitude,
+    destinationLongitude,
+  } = useLocationStore();
+
+  const region = calculateRegion({
+    userLongitude,
+    userLatitude,
+    destinationLatitude,
+    destinationLongitude,
+  });
 
   return (
-    <MapView
-      provider={PROVIDER_DEFAULT}
-      className="w-full h-full rounded-2xl"
-      tintColor="black"
-      mapType="mutedStandard"
-      showsPointsOfInterest={false}
-      // initialRegion={region}
-      showsUserLocation="light"
-    >
-      <Text>Map</Text>
-    </MapView>
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_DEFAULT}
+        style={styles.map}
+        tintColor="black"
+        mapType="mutedStandard"
+        showsPointsOfInterest={false}
+        initialRegion={region}
+        showsUserLocation={true}
+        userInterfaceStyle="light"
+      ></MapView>
+    </View>
   );
 };
 
