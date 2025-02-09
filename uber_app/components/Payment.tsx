bca49666b2d1c933446f209240ee07452db926ef
Phuc <duyphuclenguyen@gmail.com>
2025-02-09 16:46:41 -0500

    Open a payment sheet

--- uber_app/components/Payment.tsx
+++ uber_app/components/Payment.tsx
@@ -1,8 +1,44 @@
-import { View, Text } from "react-native";
+import { View, Text, Alert } from "react-native";
 import CustomButton from "./CustomButton";
+import { useStripe } from "@stripe/stripe-react-native";
+import { useState } from "react";
 
 const Payment = () => {
-  const openPaymentSheet = async () => {};
+  const { initPaymentSheet, presentPaymentSheet } = useStripe();
+  const [success, setSuccess] = useState(false);
+
+  const openPaymentSheet = async () => {
+    await initializePaymentSheet();
+
+    const { error } = await presentPaymentSheet();
+
+    if (error) {
+      Alert.alert(`Error code: ${error.code}`, error.message);
+    } else {
+      setSuccess(true);
+    }
+  };
+
+  const initializePaymentSheet = async () => {
+    const { error } = await initPaymentSheet({
+      merchantDisplayName: "Example, Inc.",
+      intentConfiguration: {
+        mode: {
+          amount: 1099,
+          currencyCode: "USD",
+        },
+        confirmHandler: confirmHandler,
+      },
+    });
+    if (error) {
+    }
+  };
+
+  const confirmHandler = async (
+    PaymentMethod,
+    shouldSavePaymentMethod,
+    intentCreationCallback
+  ) => {};
 
   return (
     <>
